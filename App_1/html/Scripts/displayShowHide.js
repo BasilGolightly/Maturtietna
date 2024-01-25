3a347a04c9cc7457a44db4743d8f885749f8677c
BasilGolightly <64554669+BasilGolightly@users.noreply.github.com>
2024-01-25 21:41:46 +0100

    GENERATED MAIL SELECT - COMPLETE DESIGN

--- App_1/html/Scripts/displayShowHide.js
+++ App_1/html/Scripts/displayShowHide.js
@@ -16,55 +16,68 @@ let settingsFrame = document.getElementById('settingsFrame');
 
 function displayMode(mode){
     
-    //check if the parameter mode is the same as the current display mode - avoid unnecessary work
-    if(mode != displayModeId){
-        //reset navbar, such that no list item is glowing
-        for(let i = 0; i < navItems.length; i++){
-            navItems[i].style.backgroundColor = "transparent";
-        }
-
-        //check which panel to show
-        switch(mode){
-            //new email
-            case 0:
-                //alert('0');
-                displayModeId = 0;
-                newMailFrame.style.display = "flex";
-                generatedFrame.style.display = "none";
-                contactsFrame.style.display = "none";
-                settingsFrame.style.display = "none";
-                document.getElementById('newMailNavItem').style.backgroundColor = "#006786";
-                break;
-            //generated
-            case 1:
-                //alert('1');
-                displayModeId = 1;
-            	generatedFrame.style.display = "flex";
-                newMailFrame.style.display = "none";
-                contactsFrame.style.display = "none";
-                settingsFrame.style.display = "none";
-                document.getElementById('generatedNavItem').style.backgroundColor = "#006786";
-                break;
-            //contacts
-            case 2:
-                //alert('2');
-                displayModeId = 2;
-                contactsFrame.style.display = "flex";
-                newMailFrame.style.display = "none";
-                generatedFrame.style.display = "none";
-                settingsFrame.style.display = "none";
-                document.getElementById('contactsNavItem').style.backgroundColor = "#006786";
-                break;
-            //settings
-            case 3:
-                //alert('3');
-                displayModeId = 3;
-                settingsFrame.style.display = "flex";
-                contactsFrame.style.display = "none";
-                newMailFrame.style.display = "none";
-                generatedFrame.style.display = "none";
-                document.getElementById('settingsNavItem').style.backgroundColor = "#006786";
-                break;
-        }
+    //reset navbar, such that no list item is glowing
+    for(let i = 0; i < navItems.length; i++){
+        navItems[i].style.backgroundColor = "transparent";
     }
+
+    //check which panel to show
+    switch(mode){
+        //new email
+        case 0:
+            //alert('0');
+            displayModeId = 0;
+            newMailFrame.style.display = "flex";
+            generatedFrame.style.display = "none";
+            contactsFrame.style.display = "none";
+            settingsFrame.style.display = "none";
+            document.getElementById('newMailNavItem').style.backgroundColor = "#006786";
+            break;
+        //generated
+        case 1:
+            //alert('1');
+            displayModeId = 1;
+            generatedFrame.style.display = "flex";
+            document.getElementById('generatedTitleText').innerHTML = "Generated mails";
+            document.getElementById('generatedMailWrap').style.display = "none";
+            document.getElementById('generatedWrap').style.display = "flex";
+            newMailFrame.style.display = "none";
+            contactsFrame.style.display = "none";
+            settingsFrame.style.display = "none";
+            document.getElementById('generatedNavItem').style.backgroundColor = "#006786";
+            break;
+        //contacts
+        case 2:
+            //alert('2');
+            displayModeId = 2;
+            contactsFrame.style.display = "flex";
+            newMailFrame.style.display = "none";
+            generatedFrame.style.display = "none";
+            settingsFrame.style.display = "none";
+            document.getElementById('contactsNavItem').style.backgroundColor = "#006786";
+            break;
+        //settings
+        case 3:
+            //alert('3');
+            displayModeId = 3;
+            settingsFrame.style.display = "flex";
+            contactsFrame.style.display = "none";
+            newMailFrame.style.display = "none";
+            generatedFrame.style.display = "none";
+            document.getElementById('settingsNavItem').style.backgroundColor = "#006786";
+            break;
+    }
+
+    /*
+    if(mode != displayModeId){
+        
+    }*/
+}
+
+function displayModeGenerated(mailId){
+    displayMode(1);
+    document.getElementById('generatedWrap').style.display = "none";
+    document.getElementById('generatedMailWrap').style.display = "flex";
+    document.getElementById('generatedMailWrap').innerHTML = "<h1>" + mailId + "</h1>"; 
+    document.getElementById('generatedTitleText').innerHTML = "<a onclick='displayMode(1)' href='#' class='generatedTitleLink'>Generated mails</a> > Mail " + mailId;
 }
