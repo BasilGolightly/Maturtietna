135d3114ef5b4a20174380ed0baf059288b913f9
BasilGolightly <64554669+BasilGolightly@users.noreply.github.com>
2024-01-27 13:12:05 +0100

    MAIL AND CONTACT DISPLAY SCRIPT

--- App_1/html/Scripts/displayShowHide.js
+++ App_1/html/Scripts/displayShowHide.js
@@ -38,7 +38,9 @@ function displayMode(mode){
             //alert('1');
             displayModeId = 1;
             generatedFrame.style.display = "flex";
-            document.getElementById('generatedTitleText').innerHTML = "Generated mails";
+            document.getElementById('generatedTitleText').innerHTML = "Generated mails <span class='generatedTitleCount' id='generatedTitleCount'></span>";
+            let generatedMails = document.getElementsByClassName('generatedLetter');
+            document.getElementById('generatedTitleCount').innerHTML = "(" + generatedMails.length + ")";
             document.getElementById('generatedMailWrap').style.display = "none";
             document.getElementById('generatedWrap').style.display = "flex";
             newMailFrame.style.display = "none";
@@ -54,7 +56,14 @@ function displayMode(mode){
             newMailFrame.style.display = "none";
             generatedFrame.style.display = "none";
             settingsFrame.style.display = "none";
+            let contacts = document.getElementsByClassName('contactFrame');
+            document.getElementById('countContacts').innerHTML = contacts.length;
+            document.getElementById('contactImg').src = "../pictures/white_pfp.png";
             document.getElementById('contactsNavItem').style.backgroundColor = "#006786";
+            document.getElementById('contactsHeadTitleText').innerHTML = "Contacts";
+            document.getElementById('selectedContact').style.display = 'none';
+            document.getElementById('addContact').style.display = 'none';
+            document.getElementById('contactList').style.display = 'flex';
             break;
         //settings
         case 3:
@@ -76,8 +85,40 @@ function displayMode(mode){
 
 function displayModeGenerated(mailId){
     displayMode(1);
+
     document.getElementById('generatedWrap').style.display = "none";
     document.getElementById('generatedMailWrap').style.display = "flex";
     document.getElementById('generatedMailWrap').innerHTML = "<h1>" + mailId + "</h1>"; 
-    document.getElementById('generatedTitleText').innerHTML = "<a onclick='displayMode(1)' href='#' class='generatedTitleLink'>Generated mails</a> > Mail " + mailId;
+    document.getElementById('generatedTitleText').innerHTML = "<a onclick='displayMode(1)' href='#' class='titleLink'>Generated mails</a> > Mail " + mailId;
+}
+
+function displayModeContacts(contactId){
+    displayMode(2);
+
+    //get selected contact's name
+    let name = document.getElementById('contact' + contactId).innerHTML;
+    
+    //rewrite title text to "contacts > name"
+    document.getElementById('contactsHeadTitleText').innerHTML = "<a class='titleLink' href='#' onclick='displayMode(2)'>Contacts</a> > " + name;
+    document.getElementById('selectedContact').innerHTML = name.trim();
+
+    //display selected Contact Wrap, hide contact list and add contact
+    document.getElementById('contactList').style.display = 'none';
+    document.getElementById('addContact').style.display = 'none';
+    document.getElementById('selectedContact').style.display = 'flex';
+}
+
+function displayModeAddContacts(){
+    displayMode(2);
+
+    //rewrite title text to "contacts > add"
+    document.getElementById('contactsHeadTitleText').innerHTML = "<a class='titleLink' href='#' onclick='displayMode(2)'>Contacts</a> > Add";
+
+    //change title icon to add contact
+    document.getElementById('contactImg').src = "../pictures/white_pfp_hover.png";
+
+    //display add contact, hide contact list and selected contact wrap
+    document.getElementById('contactList').style.display = 'none';
+    document.getElementById('selectedContact').style.display = 'none';
+    document.getElementById('addContact').style.display = 'flex';
 }
