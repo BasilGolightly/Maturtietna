39b777c0a3e7013368726db4c5f1d867fb2a6c40
BasilGolightly <64554669+BasilGolightly@users.noreply.github.com>
2024-02-10 23:41:43 +0100

    login front end finished, started register

--- App_1/Scripts/login.js
+++ App_1/Scripts/login.js
@@ -1,5 +1,6 @@
 let loginDisplayMode = 0;
 let accounts = document.getElementsByClassName('accountWrap');
+let accList = document.getElementById('accountsList');
 let loginPage = document.getElementById('loginPromptWrap');
 let loginNameOut = document.getElementById('loginName');
 
@@ -7,20 +8,33 @@ function loginDisplay(mode, accId){
     switch(mode){
         //show account list
         case 0:
+            loginNameOut.innerHTML = "";
+            //document.getElementById('loginTitle').innerHTML = `Login`;
             loginPage.style.display = "none";
-            for(let i = 0; accounts.length; i++){
+            /*for(let i = 0; accounts.length; i++){
                 accounts[i].style.display = "flex";
-            }
+            }*/
+            accList.style.display = "flex";
             loginDisplayMode = 0;
             break;
         //password prompt - selected account
         case 1:
-            let loginNameIn = document.getElementById('accountName' + accId);
-            for(let i = 0; accounts.length; i++){
+            /*for(let i = 0; accounts.length; i++){
                 accounts[i].style.display = "none";
+            }*/
+            if(accId > -1){
+                loginDisplayMode = 1;
+                accList.style.display = "none";
+                loginPage.style.display = "flex";
+                let username = document.getElementById('accountName' + accId).innerHTML.trim();
+                //console.log(username)
+                //document.getElementById('loginTitle').innerHTML = `Login`;
+                loginNameOut.innerHTML = 'for ' + username;
             }
-            loginPage.style.display = "flex";
-            loginDisplayMode = 1;
+            else{
+                loginDisplay(0, -1);
+            }
+            
             break;
     }
 }
