8434bb7774fcd47381aa6d4c4ff5493625dba973
BasilGolightly <64554669+BasilGolightly@users.noreply.github.com>
2024-02-26 15:02:04 +0100

    preload script fixed

--- App_1/preload.js
+++ App_1/preload.js
@@ -403,20 +403,21 @@ function readLoginFile(){
 
 let displayModeId = -1;
 let selectedMailId = 0;
-let date = new Date();
-date.setFullYear(date.getFullYear() - 10);
-document.getElementById('addContactDOB').max = date.getFullYear + "-" + date.getMonth() + "-" + date.getDate();
 
-let navItems = document.getElementsByClassName('listItem');
-
-let newMailFrame = document.getElementById('newMailFrame');
-let generatedFrame = document.getElementById('generatedFrame');
-let contactsFrame = document.getElementById('contactsFrame');
-let settingsFrame = document.getElementById('settingsFrame');
-let backgroundNavColor = "rgb(35, 35, 35)";
 
 function displayMode(mode){
-    
+    let date = new Date();
+    date.setFullYear(date.getFullYear() - 10);
+    document.getElementById('addContactDOB').max = date.getFullYear + "-" + date.getMonth() + "-" + date.getDate();
+
+    let navItems = document.getElementsByClassName('listItem');
+
+    let newMailFrame = document.getElementById('newMailFrame');
+    let generatedFrame = document.getElementById('generatedFrame');
+    let contactsFrame = document.getElementById('contactsFrame');
+    let settingsFrame = document.getElementById('settingsFrame');
+    let backgroundNavColor = "rgb(35, 35, 35)";
+
     //reset navbar, such that no list item is glowing
     for(let i = 0; i < navItems.length; i++){
         navItems[i].style.backgroundColor = "transparent";
