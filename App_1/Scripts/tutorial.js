e566b2b5489e9d8bf80887f9dde5e4ddb73245f5
BasilGolightly <basilgolightly14530@gmail.com>
2024-03-05 09:30:48 +0100

    čiščenje kode + ikona

--- App_1/Scripts/tutorial.js
+++ App_1/Scripts/tutorial.js
@@ -1,13 +1,40 @@
+let showGenerate = true;
+let showContact = true;
+
 function hideGenerate(){
     let generateChapter = document.getElementById('generateChapter');
-    alert("ba");
+    let generateBtn = document.getElementById('tutorialChapterBtn');
+    //alert("ba");
 
-    //show
-    if(generateChapter.style.display == 'none'){
-        generateChapter.style.display == 'block';
-    }
     //hide
+    if(showGenerate){
+        generateChapter.style.display = 'none';
+        generateBtn.innerHTML = `<img src="pictures/arrow_up.png" class="tutorialArrowIcon" width="" height=""> Generating your first mail`;
+        showGenerate = false;
+    }
+    //show
     else{
-        generateChapter.style.display == 'none';
+        generateChapter.style.display = 'grid';
+        generateBtn.innerHTML = `<img src="pictures/arrow_down.png" class="tutorialArrowIcon" width="" height=""> Generating your first mail`;
+        showGenerate = true;
+    }
+}
+
+function hideContact(){
+    let contactChapter = document.getElementById('contactChapter');
+    let contactBtn = document.getElementById('tutorialContactBtn');
+    //alert("ba");
+
+    //hide
+    if(showContact){
+        contactChapter.style.display = 'none';
+        contactBtn.innerHTML = `<img src="pictures/arrow_up.png" class="tutorialArrowIcon" width="" height=""> Contacts`;
+        showContact = false;
+    }
+    //show
+    else{
+        contactChapter.style.display = 'grid';
+        contactBtn.innerHTML = `<img src="pictures/arrow_down.png" class="tutorialArrowIcon" width="" height=""> Contacts`;
+        showContact = true;
     }
 }
